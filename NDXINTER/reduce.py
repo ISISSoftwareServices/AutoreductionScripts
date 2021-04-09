903043317649174decdbc87ec193b7a3e6cff56d
RebeccaBecky <rebecca.harding@stfc.ac.uk>
2021-04-09 17:26:37 +0100

    Add parsing settings from JSON and reading angle

--- NDXINTER/reduce.py
+++ NDXINTER/reduce.py
@@ -17,32 +17,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import reduce_vars as web_var
 import os
+import json
 
 def main(input_file, output_dir):
     standard_params = web_var.standard_vars
     advanced_params = web_var.advanced_vars
+
     config['defaultsave.directory'] = output_dir
+
+    angle = get_angle()
+    analysis_mode,first_transmission_run_list,second_transmission_run_list,transmission_processing_instructions,processing_instructions,start_overlap,end_overlap,monitor_integration_wavelength_min,monitor_integration_wavelength_max,monitor_background_wavelength_min,monitor_background_wavelength_max,wavelength_min,wavelength_max,i_zero_monitor_index,detector_correction_type = parse_json_settings(angle)
 
     alg=AlgorithmManager.create("ReflectometryISISLoadAndProcess")
     properties = {
-    "InputRunList" : standard_params['input_run_list'],
-    "FirstTransmissionRunList" : standard_params['first_transmission_run_list'],
-    "SecondTransmissionRunList" : standard_params['second_transmission_run_list'],
-    "ThetaIn" : standard_params['theta_in'],
-    "DetectorCorrectionType" : standard_params['detector_correction_type'],
-    "MonitorBackgroundWavelengthMin" : standard_params['monitor_background_wavelength_min'],
-    "MonitorBackgroundWavelengthMax" : standard_params['monitor_background_wavelength_max'],
-    "MonitorIntegrationWavelengthMin" : standard_params['MonitorIntegrationWavelengthMin'],
-    "MonitorIntegrationWavelengthMax" : standard_params['MonitorIntegrationWavelengthMax'],
-    "WavelengthMin" : standard_params['WavelengthMin'],
-    "WavelengthMax" : standard_params['WavelengthMax'],
-    "I0MonitorIndex" : standard_params['IZeroMonitorIndex'],
-    "AnalysisMode" : standard_params['analysis_mode'],
-    "StartOverlap" : standard_params['StartOverlap'],
-    "EndOverlap" : standard_params['EndOverlap'],
-    "TransmissionProcessingInstructions" : standard_params['transmission_processing_instructions'],
-    "ProcessingInstructions" : standard_params['processing_instructions']
+    "InputRunList" : input_file,
+    "FirstTransmissionRunList" : first_transmission_run_list,
+    "SecondTransmissionRunList" : second_transmission_run_list,
+    "ThetaIn" : angle,
+    "DetectorCorrectionType" : detector_correction_type,
+    "MonitorBackgroundWavelengthMin" : monitor_background_wavelength_min,
+    "MonitorBackgroundWavelengthMax" : monitor_background_wavelength_max,
+    "MonitorIntegrationWavelengthMin" : monitor_integration_wavelength_min,
+    "MonitorIntegrationWavelengthMax" : monitor_integration_wavelength_max,
+    "WavelengthMin" : wavelength_min,
+    "WavelengthMax" : wavelength_max,
+    "I0MonitorIndex" : i_zero_monitor_index,
+    "AnalysisMode" : analysis_mode,
+    "StartOverlap" : start_overlap,
+    "EndOverlap" : end_overlap,
+    "TransmissionProcessingInstructions" : transmission_processing_instructions,
+    "ProcessingInstructions" : processing_instructions
     }
+
     alg.setProperties(properties)
     alg.execute()
 
@@ -52,6 +58,80 @@ def main(input_file, output_dir):
     SaveNexus(OutputWorkspace, os.path.join(output_dir, OutputWorkspace+".nxs"))
     SaveNexus(OutputWorkspaceBinned, os.path.join(output_dir, OutputWorkspaceBinned+".nxs"))
 
+def get_angle():
+    ws=Load('INTER61668')
+    title = (ws.run().getProperty('run_title').value)
+    angle = title.split(" th=")[1].split()[0]
+    return angle
+
+def parse_json_settings(angle):
+    """
+    Autoreduction get settings from JSON file
+    """
+    json_input = r"C:\Users\wyf59278\Documents\repos\reflectometry_reduction\settings.json"
+
+    with open(json_input, "r") as read_file:
+        data = json.load(read_file)
+
+    experimentView = data["experimentView"]
+
+    if experimentView["analysisModeComboBox"] == 1:
+        analysis_mode = "MultiDetectorAnalysis"
+    elif experimentView["analysisModeComboBox"] == 0:
+        analysis_mode = "PointDetectorAnalysis"
+    else:
+        raise Exception # If the value isn't 1 or 0 then it isn't valid
+
+    perAngleDefaults = experimentView["perAngleDefaults"]
+    rows = perAngleDefaults["rows"]
+
+    # This looks for the run angle and set other parameters accordingly
+    angle_found = False
+    for i in rows:
+        if i[0] == angle:
+            angle_found = True
+            first_transmission_run_list = i[1]
+            second_transmission_run_list = i[2]
+            transmission_processing_instructions = i[3]
+            processing_instructions = i[8]
+            break
+
+    # This is the default case
+    if not angle_found:
+        for i in rows:
+            if i[0] == "":
+                angle_found = True
+                first_transmission_run_list = i[1]
+                second_transmission_run_list = i[2]
+                transmission_processing_instructions = i[3]
+                processing_instructions = i[8]
+                break
+
+    if not angle_found:
+        raise Exception # Excpetion for if neither a pre-defined angle or the default case are found
+
+    start_overlap = experimentView["startOverlapEdit"]
+
+    end_overlap = experimentView["endOverlapEdit"]
+
+    instrumentView = data["instrumentView"]
+
+    monitor_integration_wavelength_min = instrumentView["monIntMinEdit"]
+    monitor_integration_wavelength_max = instrumentView["monIntMaxEdit"]
+    monitor_background_wavelength_min = instrumentView["monBgMinEdit"]
+    monitor_background_wavelength_max = instrumentView["monBgMaxEdit"]
+    wavelength_min = instrumentView["lamMinEdit"]
+    wavelength_max = instrumentView["lamMaxEdit"]
+    i_zero_monitor_index = instrumentView["I0MonitorIndex"]
+
+    if instrumentView["detectorCorrectionTypeComboBox"] == 1:
+        detector_correction_type = "RotateAroundSample"
+    elif instrumentView["detectorCorrectionTypeComboBox"] == 0:
+        detector_correction_type = "VerticalShift"
+    else:
+        raise Exception # If the value isn't 1 or 0 then it isn't valid
+
+    return analysis_mode,first_transmission_run_list,second_transmission_run_list,transmission_processing_instructions,processing_instructions,start_overlap,end_overlap,monitor_integration_wavelength_min,monitor_integration_wavelength_max,monitor_background_wavelength_min,monitor_background_wavelength_max,wavelength_min,wavelength_max,i_zero_monitor_index,detector_correction_type
+
 if __name__ == "__main__":
     main('', '')
-
--- NDXINTER/reduce_vars.py
+++ NDXINTER/reduce_vars.py
@@ -1,22 +1,23 @@
 standard_vars = {
     'input_run_list' : "INTER00061667",
+    'theta_in': 0.7,
+
+    'analysis_mode': "MultiDetectorAnalysis",
     'first_transmission_run_list' : "INTER00061705",
     'second_transmission_run_list' : "INTER00061669",
-    'theta_in': 0.7,
-    'detector_correction_type': "RotateAroundSample",
-    'monitor_background_wavelength_min': 0.0,
-    'monitor_background_wavelength_max': 1.0,
-    'analysis_mode': "MultiDetectorAnalysis",
     'transmission_processing_instructions': "76-85",
-    'processing_instructions': "80-84"
-    'MonitorIntegrationWavelengthMin' : 4.0,
-    'MonitorIntegrationWavelengthMax' : 10.0,
-    'WavelengthMin' : 1.5,
-    'WavelengthMax' : 17.0,
-    'StartOverlap' : 10.0,
-    'EndOverlap' : 12.0,
-    'IZeroMonitorIndex' : 2
+    'processing_instructions': "80-84",
+    'start_overlap' : 10.0,
+    'end_overlap' : 12.0
 }
 advanced_vars = {
+    'monitor_integration_wavelength_min' : 4.0,
+    'monitor_integration_wavelength_max' : 10.0,
+    'monitor_background_wavelength_min': 17.0,
+    'monitor_background_wavelength_max': 18.0,
+    'wavelength_min' : 1.5,
+    'wavelength_max' : 17.0,
+    'i_zero_monitor_index' : 2,
+    'detector_correction_type': "VerticalShift"
 }
 
