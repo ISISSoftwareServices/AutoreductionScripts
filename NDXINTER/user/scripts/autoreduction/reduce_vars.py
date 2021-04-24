514642608f6a5f4ca68cafa3eb8d10e34a22efde
ISIS Autoreduce Team <isisreduce@stfc.ac.uk>
2021-04-24 01:00:04 +0100

    Reduction files for 2021-04-24

--- NDXINTER/user/scripts/autoreduction/reduce_vars.py
+++ NDXINTER/user/scripts/autoreduction/reduce_vars.py
@@ -1,14 +1,20 @@
 standard_vars = {
-    'example standard var' : [1.0, 2.0, 3.0], # example comment
+    'path_to_json_settings_file': "/isis/NDXINTER/user/scripts/autoreduction/settings.json"
+    # 'analysis_mode': "MultiDetectorAnalysis",
+    # 'first_transmission_run_list' : "INTER00061705",
+    # 'second_transmission_run_list' : "INTER00061669",
+    # 'transmission_processing_instructions': "76-85",
+    # 'processing_instructions': "80-84",
+    # 'start_overlap' : 10.0,
+    # 'end_overlap' : 12.0
 }
 advanced_vars = {
-    'example advanced var' : '5',
-}
-variable_help = {
-    'standard_vars' : {
-        'example standard var' : "The list of a, b, c"
-    },
-    'advanced_vars' : {
-        'example advanced var' : "example comment"
-    },
+    # 'monitor_integration_wavelength_min' : 4.0,
+    # 'monitor_integration_wavelength_max' : 10.0,
+    # 'monitor_background_wavelength_min': 17.0,
+    # 'monitor_background_wavelength_max': 18.0,
+    # 'wavelength_min' : 1.5,
+    # 'wavelength_max' : 17.0,
+    # 'i_zero_monitor_index' : 2,
+    # 'detector_correction_type': "VerticalShift"
 }
